6a10288a233a71cd1abb533a653bc90d37cca0ea
quarzasiphix <quarrysiphix@gmail.com>
2024-04-13 18:40:19 +0100

    fixes

--- general/scripts/setup.sh
+++ general/scripts/setup.sh
@@ -1,13 +1,14 @@
 dir="/var/www/scripts"
 
+sudo rm -R $dir/downloader
 sudo mkdir $dir/downloader
 sudo chmod 777 -R $dir
 
-dwldir="$dir/new/downloader"
+dwldir="$dir/downloader"
 dwlurl="https://raw.githubusercontent.com/quarzasiphix/server-setup/master/general/scripts/downloader/download.sh"
 #remove previous if exists
 sudo rm $dwldir/download.sh
-sudo curl -o "$dwldir" "$dwlurl"
+sudo curl -o "$dwldir/download.sh" "$dwlurl"
 sudo chmod +x $dwldir/download.sh
 
 echo
