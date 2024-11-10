7cd6eb115ffc2cb28be6dffc065cb3a0641c7332
quarzasiphix <quarrysiphix@gmail.com>
2024-11-10 17:51:06 +0000

    fixing wpcli

--- source/manager/site/setupwp.sh
+++ source/manager/site/setupwp.sh
@@ -34,19 +34,6 @@ SetupWP() {
     sudo tar -xvzf latest.tar.gz --strip-components=1 -C "$dir" > /dev/null
     echo "finished extracting wp files.. setting up perms"
 
-        # Use WP-CLI to configure WordPress
-    # Force https and allow 512mb file size
-    dbprefix="${name:0:1}${name: -1}_"
-    sudo wp core config --allow-root --path="$dir" --dbname="$name" --dbuser="$name" --dbpass="$password" --dbprefix="$dbprefix" --dbhost="localhost" --extra-php <<PHP
-    define( 'WP_MEMORY_LIMIT', '512M' );
-    \$_SERVER['HTTPS'] = 'on';
-PHP
-    sudo chmod 600 "$dir/wp-config.php" > /dev/null
-
-    echo
-    sudo chown -R www-data:www-data "$dir"
-    sudo chmod -R 755 "$dir"
-
     echo
     echo "setting up database"
     echo
@@ -61,6 +48,20 @@ PHP
     FLUSH PRIVILEGES;
     \q
 EOF
+
+
+    # Use WP-CLI to configure WordPress
+    # Force https and allow 512mb file size
+    dbprefix="${name:0:1}${name: -1}_"
+    sudo wp core config --allow-root --path="$dir" --dbname="$name" --dbuser="$name" --dbpass="$password" --dbprefix="$dbprefix" --dbhost="localhost" --extra-php <<PHP
+    define( 'WP_MEMORY_LIMIT', '512M' );
+    \$_SERVER['HTTPS'] = 'on';
+PHP
+    sudo chmod 600 "$dir/wp-config.php" > /dev/null
+
+    echo
+    sudo chown -R www-data:www-data "$dir"
+    sudo chmod -R 755 "$dir"
 
     echo
     echo "Setting up Nginx"
